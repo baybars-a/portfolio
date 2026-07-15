b1ac9a9ce5a91d06ff42743c4e79c23e6d3a9804
baybarsbaha <baybarsbaha42@gmail.com>
2026-07-15 15:11:00 +0400

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   viewportFit: "cover",
-  themeColor: "#0d0602",
+  themeColor: "#000000",
 };
 
 export default function RootLayout({
